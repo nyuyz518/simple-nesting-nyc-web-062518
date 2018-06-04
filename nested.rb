eba19dbdf95bfbb99ae8b270e1ecef24d8d0b4a3
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-04 15:01:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
 
 def hopper
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -15,13 +14,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+	return programmer_hash[:alan_kay][:known_for]
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -63,6 +61,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+}
 
 	programmer_hash = 
  		{
@@ -80,7 +82,6 @@ def adding_matz
         }
      }
 
-    
 end
 
 def changing_alan
