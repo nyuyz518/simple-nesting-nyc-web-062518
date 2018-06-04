9102c226cec6ab661955fb3540758cdd4e1b23ff
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-04 15:16:52 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
          :known_for => "Ruby",
          :languages => ["LISP", "C"]
    }
-   programmer_hash
+   return programmer_hash
 
 end
 
@@ -107,7 +107,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -127,5 +127,5 @@ def adding_to_dennis
         }
      }
    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-   programmer_hash
+   return programmer_hash
 end
